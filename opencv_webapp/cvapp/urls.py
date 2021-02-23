009808c399447b8bc5af1b8eab4933f726627d22
Mactto <shc9928@gmail.com>
2021-02-23 16:54:33 +0900

    image upload

--- opencv_webapp/cvapp/urls.py
+++ opencv_webapp/cvapp/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import *
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', first_view, name="first_view"),
-]
+    path('uimage/', uimage, name="uimage"),
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
